ebdaebef27138193af1092740e491daf431ef2db
Stephen Smith <stephen.smith@microfocus.com>
2016-01-22 14:58:33 -0700

    - testing 1234

--- aliases.sh
+++ aliases.sh
@@ -21,7 +21,6 @@ alias branches='git branch -va'
 alias vitig='vim ~/.tigrc'
 alias crap='$(thefuck $(fc -ln -1))' # A bash autocorrect utility, i didn't name it
 
-# @commit: I think this doesn't keep a clean repo because it alters the files after commit.
+# @commit: testing 1234
 alias maven='mvn clean compile assembly:single'
 alias jarup='cd target && scp -rpC IAAMAuto-1.0SNAPSHOT-jar-with-dependencies.jar root@10.204.130.109:/srv/ftp/auto/auto-sspr-4.0.0.jar'
-
